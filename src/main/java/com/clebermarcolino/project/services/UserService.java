ccf4e6ccd3dbe140d916b316cb7af6bda966d201
clebermarcolino <clebermarcolinogba@gmail.com>
2025-05-13 17:22:18 -0300

    user delete

--- src/main/java/com/clebermarcolino/project/services/UserService.java
+++ src/main/java/com/clebermarcolino/project/services/UserService.java
@@ -26,4 +26,8 @@ public class UserService {
     public User insert(User object) {
         return userRepository.save(object);
     }
+
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
 }
