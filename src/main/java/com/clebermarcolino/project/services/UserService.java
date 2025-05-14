f051649426e73587bc6c2d83e1810eafe27f0e7a
clebermarcolino <clebermarcolinogba@gmail.com>
2025-05-14 17:23:25 -0300

    exception handling - findById

--- src/main/java/com/clebermarcolino/project/services/UserService.java
+++ src/main/java/com/clebermarcolino/project/services/UserService.java
@@ -2,6 +2,7 @@ package com.clebermarcolino.project.services;
 
 import com.clebermarcolino.project.entities.User;
 import com.clebermarcolino.project.repositories.UserRepository;
+import com.clebermarcolino.project.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> object = userRepository.findById(id);
-        return object.get();
+        return object.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User object) {
