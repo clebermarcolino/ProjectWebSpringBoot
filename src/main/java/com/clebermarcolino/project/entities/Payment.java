dc1d07d38f555a92f022242a22b63c92e26be48a
clebermarcolino <clebermarcolinogba@gmail.com>
2025-05-17 17:25:39 -0300

    classe payment devidamente comentada.

--- src/main/java/com/clebermarcolino/project/entities/Payment.java
+++ src/main/java/com/clebermarcolino/project/entities/Payment.java
@@ -7,19 +7,19 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
-@Entity
-@Table(name = "tb_payment")
+@Entity // Anotação JPA que marca esta classe como uma entidade, representando uma tabela no banco de dados.
+@Table(name = "tb_payment") // Anotação JPA que especifica o nome da tabela no banco de dados que será mapeada para esta entidade.
 public class Payment implements Serializable {
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L; // Variável estática e final que armazena um identificador único para a classe serializada. É importante para controle de versão durante a desserialização.
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id // Anotação JPA que marca o campo 'id' como a chave primária desta entidade
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Anotação JPA que especifica a estratégia de geração de valores para a chave primária. GenerationType.IDENTITY indica que a geração será feita pela coluna de identidade do banco de dados (auto incremento).
     private Long id;
     private Instant moment;
 
-    @OneToOne
-    @MapsId
-    @JsonIgnore
+    @OneToOne // Anotação JPA que define um relacionamento One-to-One com a entidade 'Order' (um pagamento para um pedido).
+    @MapsId // Anotação JPA utilizada em relacionamentos One-to-One para indicar que a chave primária desta entidade ('Payment') é também a chave estrangeira referenciando a chave primária da entidade relacionada ('Order').
+    @JsonIgnore // Anotação Jackson que indica que o atributo 'order' deve ser ignorado durante a serialização para JSON. Isso evita loops infinitos em relacionamentos bidirecionais.
     private Order order;
 
     public Payment() {
